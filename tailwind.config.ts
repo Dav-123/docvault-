5e75b5cc2e8f167ef47d8303c7a5dca8c18354c2
* <davidbriggd478@gmail.comgit config --global user.name Dav123>
2025-11-14 11:14:05 +0100

    Fixed bugs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,8 @@ import type { Config } from "tailwindcss"
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
   ],
   prefix: "",
   theme: {
